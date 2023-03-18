14622d5f204843b069bfebb0c123b8e5d736f119
vicdant1 <dantjv@outlook.com>
2023-03-18 14:18:05 -0300

    working with struct composition golang

--- hellogo/struct.go
+++ hellogo/struct.go
@@ -8,12 +8,35 @@ type Costumer struct {
 	IdCard string
 }
 
+func (costumer Costumer) PrintCostumerData() {
+	fmt.Printf("Name: %s\nEmail: %s\nId Card: %s\n", costumer.Name, costumer.Email, costumer.IdCard)
+}
+
+func (internationalCostumer InternationalCostumer) PrintCostumerData() {
+	// Note that I can use internationalCostumer.Name instead of internationalCostumer.Costumer.Name
+	fmt.Printf("\n[***** INTERNATIONAL *****]\nName: %s\nEmail: %s\nId Card: %s\nCountry: %s\n", internationalCostumer.Name, internationalCostumer.Email, internationalCostumer.IdCard, internationalCostumer.Country)
+}
+
+type InternationalCostumer struct {
+	Costumer // Kindof composition
+	Country  string
+	Name     string
+}
+
 func main() {
 	costumer := Costumer{
 		Name:   "John",
 		Email:  "john@johnices.com",
 		IdCard: "90123.123-11",
 	}
+	costumer.PrintCostumerData()
 
-	fmt.Printf("Name: %s\nEmail: %s\nId Card: %s\n", costumer.Name, costumer.Email, costumer.IdCard)
+	internationalCostumer := InternationalCostumer{
+		Costumer: costumer,
+		Country:  "USA",
+		Name:     "OVERLOADNAME",
+	}
+
+	internationalCostumer.PrintCostumerData() // intersting feature
+
 }
