5c257da143764f1366263eaef250d7e07f14c512
vicdant1 <dantjv@outlook.com>
2023-03-18 18:02:54 -0300

    working more with goroutines

--- hellogo/threads.go
+++ hellogo/threads.go
@@ -6,15 +6,20 @@ import (
 )
 
 func Counter(systype string) {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(systype, i)
 		time.Sleep(time.Second)
 	}
 }
 
 func main() {
-	go Counter("a ")
-	go Counter("b ")
+	fmt.Println("Starting execution")
+
+	go Counter("goroutine")
+	for i := 0; i < 5; i++ {
+		fmt.Println(i, "<=>")
+		time.Sleep(time.Second * 1 / 2)
+	}
 
 	time.Sleep(time.Second * 10)
 }
