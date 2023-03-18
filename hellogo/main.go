b797a1ccdc53cbf49a8e7d5a24216b41098fd642
vicdant1 <dantjv@outlook.com>
2023-03-18 12:09:38 -0300

    working with multi parameter argument

--- hellogo/main.go
+++ hellogo/main.go
@@ -7,26 +7,26 @@ import (
 )
 
 func main() {
-	resultado, err := Soma(0, 1)
+	result, err := Sum(0, 1)
 
 	if err != nil {
-		log.Fatal("Não foi possível completar a operação: " + err.Error())
+		log.Fatal("Operation problem: " + err.Error())
 	}
 
 	// Same behavior:
 	// fmt.Println(fmt.Sprintf("No problem with operation, result is %d", resultado))
-	fmt.Printf("No problem with operation, result is %d\n", resultado)
+	fmt.Printf("No problem with operation, result is %d\n", result)
 
 	noValueResult := SpecialMultiplication(71)
 	fmt.Printf("Result: %d\n", noValueResult)
+
+	SequencialSum(1, 2, 3, 3, 43, 42, 34, 23, 42, 34, 23, 42, 342, 3, 42)
 }
 
-func Soma(n1 int, n2 int) (int, error) {
-
-	soma := n1 + n2
-
-	if soma > 10 {
-		return soma, errors.New("soma maior que 10")
+func Sum(n1 int, n2 int) (int, error) {
+	sum := n1 + n2
+	if sum > 10 {
+		return sum, errors.New("Bigger than 10")
 	}
 
 	return n1 + n2, nil
@@ -36,4 +36,8 @@ func SpecialMultiplication(value int) (result int) {
 	value *= 836
 	result = value
 	return
+}
+
+func SequencialSum(x ...int) {
+	fmt.Printf("%d elements", len(x))
 }
