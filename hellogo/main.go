1b8e4396f3a7f012b577bbd9bc50a65b1acaa78f
vicdant1 <dantjv@outlook.com>
2023-03-18 12:52:46 -0300

    working with loops

--- hellogo/main.go
+++ hellogo/main.go
@@ -21,6 +21,14 @@ func main() {
 	fmt.Printf("Result: %d\n", noValueResult)
 
 	SequencialSum(1, 2, 3, 3, 43, 42, 34, 23, 42, 34, 23, 42, 342, 3, 42)
+
+	// DivideScreen()
+
+	// UnderstandingFors()
+}
+
+func DivideScreen() {
+	fmt.Println("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=")
 }
 
 func Sum(n1 int, n2 int) (int, error) {
@@ -39,5 +47,43 @@ func SpecialMultiplication(value int) (result int) {
 }
 
 func SequencialSum(x ...int) {
-	fmt.Printf("%d elements", len(x))
+	fmt.Printf("%d elementsin sequence\n", len(x))
+	var sum int
+	for _, element := range x {
+		fmt.Printf("%d ", element)
+		sum += element
+	}
+	fmt.Println()
+
+	fmt.Printf("The sum of the %d elements is %d", len(x), sum)
+}
+
+func UnderstandingFors() {
+	// Usual for
+	for i := 0; i <= 10; i++ {
+		fmt.Printf(" %d ", i)
+	}
+	fmt.Println()
+
+	DivideScreen()
+
+	// Go while
+	lowerThan10 := true
+	counter := 0
+	for lowerThan10 {
+		fmt.Printf("Current number: %d\n", counter)
+		if counter == 10 {
+			lowerThan10 = false
+		}
+		counter++
+	}
+
+	DivideScreen()
+
+	// Go foreach (reads key and value of a array)
+	intArray := [...]int{0, 1, 2, 2, 2, 3, 4, 1}
+	for key, value := range intArray {
+		fmt.Printf("Key: %d, Value: %d\n", key, value)
+	}
+
 }
