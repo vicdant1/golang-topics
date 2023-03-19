01f502c9f1942f7d5b235d1647bc98a05eb2b8ea
vicdant1 <dantjv@outlook.com>
2023-03-18 22:48:14 -0300

    working with channels

--- hellogo/channels.go
+++ hellogo/channels.go
@@ -14,17 +14,37 @@ func Write(identifier string, channel chan<- bool) {
 	channel <- true
 }
 
+func ProcessNumbers(numberChannel chan<- int) {
+	fmt.Println("Entrou Process")
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Escrevendo %d\n", i)
+		numberChannel <- i
+	}
+	fmt.Println("Saiu Process")
+
+	close(numberChannel)
+}
+
 func main() {
 	// Channels works in order to make the PIPE scheme, it allows a thread to communicate with others
 
-	cn1 := make(chan bool)
-	cn2 := make(chan bool)
+	// cn1 := make(chan bool)
+	// cn2 := make(chan bool)
 
-	go Write("a", cn1)
-	go Write("b", cn2)
+	// go Write("a", cn1)
+	// go Write("b", cn2)
 
 	// Making main (wich is a go routine as well) be blocked til cn1 & cn2 have a state change
-	<-cn1
-	<-cn2
+	// <-cn1
+	// <-cn2
+	fmt.Println("entrando main")
 
+	numberChannel := make(chan int)
+
+	go ProcessNumbers(numberChannel)
+
+	for v := range numberChannel {
+		fmt.Printf("Lendo %d\n", v)
+	}
+	fmt.Println("saindo main")
 }
