da5152ff46e9711dcfd1570f4adef264d81756f2
vicdant1 <dantjv@outlook.com>
2023-03-25 01:05:48 -0300

    reading txt

---
+++ hellogo/dup.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// this is an algorithm to catch duplicated words
+
+func main() {
+	fmt.Println("Dup Algorithm")
+
+	// string: int
+	// "brasilia": 2
+	// words := make(map[string]int)
+
+	file, err := os.Open("../static/duplicated.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("An error occured")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
