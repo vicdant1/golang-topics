e1fb9b4ebd7038623fc6ff9d90bc10c45a4bb3f1
vicdant1 <dantjv@outlook.com>
2024-05-30 19:55:03 -0300

    working on app

--- review/booking-app/main.go
+++ review/booking-app/main.go
@@ -1,49 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets uint16 = 50
 	var remainingTickets = conferenceTickets
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint16
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint16
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter your number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
+		remainingTickets -= userTickets
 
-	// arrays and slices
-	// Array (arrays in golang has fixed size)
-	// var bookings = [50]string{}
-	var bookings [50]string
+		// arrays and slices
+		// Array (arrays in golang has fixed size)
+		// var bookings = [50]string{}
+		// var bookings []string
+		// var bookings = []string{}
 
-	bookings[0] = firstName + "	" + lastName
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+		// bookings[0] = firstName + "	" + lastName
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
 
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+			names := strings.Fields(booking)
+			firstName := names[0]
+			firstNames = append(firstNames, firstName)
+		}
+
+		fmt.Printf("These are all our bookings: %v\n", firstNames)
+
+	}
 }
